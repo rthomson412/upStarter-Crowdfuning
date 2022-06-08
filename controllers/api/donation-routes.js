d7bd5d989868db163f3ba626cc344259f889200d
M0byNick <nmorihisa@gmail.com>
2022-06-08 15:48:19 -0400

    Connected Heroku and JAWSDB

--- controllers/api/donation-routes.js
+++ controllers/api/donation-routes.js
@@ -31,7 +31,7 @@ router.post("/", withAuth, async (req, res) => {
       })
       .catch((err) => {
         console.log(err);
-        res.status(400).json(err);
+        res.status(500).json(err);
       });
   }
 });
